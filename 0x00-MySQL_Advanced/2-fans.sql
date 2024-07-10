23e08eddb66eda436ac50860837c43519c7b1f21
SaidLamghari <said.lmg@gmail.com>
2024-07-10 16:49:30 +0000

    2-fans.sql

---
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -0,0 +1,8 @@
+-- Calcul du nombre total de fans
+-- par origine des groupes de métal,
+-- classé par ordre décroissant
+
+SELECT origin, SUM(nb_fans) AS nb_fans
+FROM metal_bands
+GROUP BY origin
+ORDER BY nb_fans DESC;
