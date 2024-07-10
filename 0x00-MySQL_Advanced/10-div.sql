12583b5074b22629d8b5aaa238fba7e9c73cd1de
SaidLamghari <said.lmg@gmail.com>
2024-07-10 20:27:52 +0000

    10-div.sql

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -9,19 +9,19 @@ CREATE FUNCTION SafeDiv(a INT, b INT)
 RETURNS FLOAT
 BEGIN
     -- Déclare une variable locale pour stocker le résultat de la division
-    DECLARE result FLOAT;
+    DECLARE rslt FLOAT;
 
     -- Vérifie si le dénominateur (b) est égal à 0
     IF b = 0 THEN
         -- Si b est 0, retourne 0
-        SET result = 0;
+        SET rslt = 0;
     ELSE
         -- Sinon, effectue la division a / b
-        SET result = a / b;
+        SET rslt = a / b;
     END IF;
 
     -- Retourne le résultat de la division
-    RETURN result;
+    RETURN rslt;
 END$$
 
 -- Restaure le délimiteur par défaut ;
