2d696f157d1bf4fb6e73ed1db8bc024c72ffa0c1
SaidLamghari <said.lmg@gmail.com>
2024-07-10 18:57:36 +0000

    7-average_score.sql

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -4,24 +4,22 @@
 -- met à jour le score moyen d'un étudiant.
 -- Auteur SAID LAMGHARI
 
+-- Supprime la procédure stockée ComputeAverageScoreForUser si elle existe déjà
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+-- Change le délimiteur pour pouvoir utiliser '$$' comme délimiteur de fin pour la procédure
 DELIMITER $$
-
+-- Définit la procédure stockée ComputeAverageScoreForUser avec un paramètre en entrée
 CREATE PROCEDURE ComputeAverageScoreForUser(
-    IN user_id INT
-)
+    IN user_id INT)  -- ID de l'utilisateur pour lequel calculer la moyenne
 BEGIN
-    DECLARE avg_score DECIMAL(10, 2);
-
-    -- Calculer la moyenne des scores pour l'utilisateur donné
-    SELECT AVG(score) INTO avg_score
-    FROM corrections
-    WHERE user_id = user_id;
-
-    -- Mettre à jour la colonne average_score dans la table users
-    UPDATE users
-    SET average_score = avg_score
-    WHERE id = user_id;
-
+    -- Déclare une variable locale pour stocker la moyenne des scores
+    DECLARE avg_score FLOAT;
+    
+    -- Calcule la moyenne des scores pour l'utilisateur donné
+    SET avg_score = (SELECT AVG(score) FROM corrections AS C WHERE C.user_id = user_id);
+    
+    -- Met à jour la table 'users' avec la moyenne calculée
+    UPDATE users SET average_score = avg_score WHERE id = user_id;
 END$$
-
+-- Restaure le délimiteur par défaut ';'
 DELIMITER ;
