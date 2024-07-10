437e3280aca05ac3c3b693367739116ddcae2641
SaidLamghari <said.lmg@gmail.com>
2024-07-10 17:24:47 +0000

    3-glam_rock.sql

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -4,7 +4,7 @@
 -- Auteur SAID LAMGHARI
 
 SELECT band_name,
-       IF(split != 0 AND formed != 0, (2022 - formed) - split, 0) as lifespan
+IF(split != 0 AND formed != 0, (2022 - formed) - split, 0) as lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC, band_name;
