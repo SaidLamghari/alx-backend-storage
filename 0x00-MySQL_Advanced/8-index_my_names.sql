8cc11b85595d3500e01941896f139b7829681fa7
SaidLamghari <said.lmg@gmail.com>
2024-07-10 19:48:06 +0000

    8-index_my_names.sql

---
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -0,0 +1,10 @@
+-- Importer le dump de la table names si ce n'est pas déjà fait
+-- Exécutez la commande suivante pour importer :
+-- cat names.sql | mysql -uroot -p holberton
+-- Auteur SAID LAMGHARI
+
+-- Création de l'index idx_name_first sur la première lettre du champ name
+CREATE INDEX idx_name_first ON names (LEFT(name, 1));
+
+-- Afficher les index pour vérification
+SHOW INDEX FROM names;
