8b98db544200af4bd9d84d2750b08babb4710eb0
SaidLamghari <said.lmg@gmail.com>
2024-07-11 18:25:51 +0000

    101-average_weighted_score.sql

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -13,13 +13,13 @@ DELIMITER $$
 CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
 BEGIN
     -- Étape 1: Crée une table temporaire pour stocker les scores moyens pondérés calculés
-    CREATE TEMPORARY TABLE IF NOT EXISTS temp_avg_weighted_scores (
+    CREATE TEMPORARY TABLE IF NOT EXISTS temp_avg_wghtd_scores (
         user_id INT PRIMARY KEY,
         avg_weighted_score FLOAT
     );
 
     -- Étape 2: Calcul des scores moyens pondérés pour chaque utilisateur et insertion dans la table temporaire
-    INSERT INTO temp_avg_weighted_scores (user_id, avg_weighted_score)
+    INSERT INTO temp_avg_wghtd_scores (user_id, avg_weighted_score)
     SELECT C.user_id, SUM(C.score * P.weight) / SUM(P.weight) AS avg_weighted_score
     FROM corrections AS C
     JOIN projects AS P ON C.project_id = P.id
@@ -27,11 +27,11 @@ BEGIN
 
     -- Étape 3: Mise à jour des scores moyens pondérés dans la table users à partir de la table temporaire
     UPDATE users AS U
-    JOIN temp_avg_weighted_scores AS T ON U.id = T.user_id
+    JOIN temp_avg_wghtd_scores AS T ON U.id = T.user_id
     SET U.average_score = T.avg_weighted_score;
 
     -- Étape 4: Supprime la table temporaire une fois la mise à jour terminée pour libérer l'espace mémoire
-    DROP TEMPORARY TABLE IF EXISTS temp_avg_weighted_scores;
+    DROP TEMPORARY TABLE IF EXISTS temp_avg_wghtd_scores;
 END
 $$
 
