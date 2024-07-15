7ad9ce487365aeca7644d0620e6b44a3eb86b2fc
SaidLamghari <said.lmg@gmail.com>
2024-07-15 16:05:02 -0700

    9-insert_school.py

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Module to define a function that inserts a
+document in a MongoDB collection based on kwargs.
+Auteur SAID LAMGHARI
+"""
+from pymongo.collection import Collection
+
+
+def insert_school(mongo_collection: Collection, **kwargs) -> str:
+    """
+    Insert a new document into the MongoDB
+    collection with the provided kwargs.
+    Return the _id of the newly inserted document.
+    """
+    # Insert a new document with the provided kwargs
+    rslt = mongo_collection.insert_one(kwargs)
+
+    # Return the _id of the newly inserted document
+    return str(rslt.inserted_id)
