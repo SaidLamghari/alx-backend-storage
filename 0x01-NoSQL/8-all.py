60995c4273cd18ec862ab5d8905b513496a1d036
SaidLamghari <said.lmg@gmail.com>
2024-07-15 15:59:06 -0700

    8-all.py

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -4,11 +4,8 @@ Module to define a function that
 lists all documents in a MongoDB collection.
 Auteur SAID LAMGHARI
 """
-from pymongo.collection import Collection
 
-def list_all(mongo_collection: Collection) -> list:
-    """
-    List all documents in the given MongoDB collection.
-    """
-    dcumts = list(mongo_collection.find())
-    return dcumts
+
+def list_all(mongo_collection):
+    """ Function"""
+    return list(mongo_collection.find())
