24c8c0495e08811858e8c7ca028da569367c8459
SaidLamghari <said.lmg@gmail.com>
2024-07-15 16:26:25 -0700

    10-update_topics.py

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Module to define a function that updates topics of a school
+document based on name in a MongoDB collection.
+Auteur SAID LAMGHRI
+"""
+from pymongo.collection import Collection
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    Update topics of a school document in MongoDB collection based on name.
+    """
+    # Update the document matching the name with new topics
+    rslt = mongo_collection.update_one(
+        {"name": name},
+        {"$set": {"topics": topics}}
+    )
+
+    # Print a message indicating how many documents were modified (optional)
+    print("Modified {} documents".format(rslt.modified_count))
